d8eeeb64bcaada874bcea32c81b740adc335c896
2360527568 <2360527568@qq.com>
2019-06-06 08:46:58 +0800

    feat: 整理下

--- src/App.js
+++ src/App.js
@@ -5,19 +5,25 @@ import Index from './header/header'
 import LikeButton from './like/like'
 import CommentApp from './comments/comment'
 import Lifecircle from './lifecircle/lifecircle'
+import Clock from './clock/clock'
 
 function App() {
   return (
     <div className="App">
       <Index />
+      <hr/>
+      <h1>props 配置示例</h1>
       <div>props 配置</div>
       <LikeButton likedText='已赞' unlikedText='赞'/>
       <div>没有 props 配置</div>
       <LikeButton />
-      <div>评论app</div>
+      <hr/>
+      <h1>评论app</h1>
       <CommentApp />
-      <div>react 的生命周期</div>
+      <hr/>
+      <h1>react 的生命周期</h1>
       <Lifecircle />
+      <Clock />
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello React</p>
